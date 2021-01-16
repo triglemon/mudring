95df1855652c4e0fa38eb7d042bdba5beeba2c87
RashadArbab <rashad.arbab@gmail.com>
2021-01-16 15:55:32 -0500

    server running now

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express'); //handles routing and the urls for us 
-const morgan =require('morgan'); 
+const morgan = require('morgan'); 
 const router = express.Router() ; 
 const app = express() ; 
 const api = require('./api'); 
 
 
 app.use(morgan('tiny')); 
-app.use('express.json')
+
 
 app.use('/api' , api); 
 
@@ -20,4 +20,3 @@ app.listen(PORT , (err)=>{
         console.log(`server listening on port: ${PORT}`); 
     }
 })
-morg
