fa76d9d6c2bb813e3ecce1850031ecaf28475142
triglemon <robertzengheliu@gmail.com>
2021-01-16 21:16:05 -0500

    face overlay works

--- mudring-react/src/App.js
+++ mudring-react/src/App.js
@@ -1,21 +1,63 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
+import * as faceapi from 'face-api.js'
 import './App.css';
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Home from './components/Home';
-import {BrowserRouter , Switch, Route, Redirect} from 'react-router-dom'; 
+
 function App() {
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const [initializing, setInitializing] = useState(false)
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models'
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo)
+    }
+    loadModels();
+  }, [])
+
+  const startVideo = () => {
+    navigator.getUserMedia({
+      video : {}
+    }, stream => videoRef.current.srcObject = stream,
+    function (){console.warn("Error getting audio stream from getUserMedia")})
+  }
+
+  const handleVideoOnPlay = () => {
+    setInterval(async() => {
+      if(initializing){
+        setInitializing(false);
+      }
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+      const displaySize = {
+        width: videoWidth,
+        height: videoHeight
+      }
+      faceapi.matchDimensions(canvasRef.current, displaySize);
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvasRef.current.getContext('2d').clearRect(0,0, videoWidth, videoHeight);
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+      console.log(detections)
+    }, 100)
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact >
-            <Redirect to="/home" />
-          </Route>
-          <Route path='/home'>
-            <Home/> 
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <span>{initializing ? 'Initializing' : 'Ready'}</span>
+      <div>
+        <video ref={videoRef} autoPlay muted  height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay}/>
+        <canvas ref={canvasRef} />
+      </div>
     </div>
   );
 }
